4a10052c70021ebb52e8fd814071f0c8a0f5b369
erikaCutugno <erikucciapop@gmail.com>
2024-12-03 15:28:07 +0100

    feat: order exe dadi

--- js-dadi/script.js
+++ js-dadi/script.js
@@ -5,10 +5,13 @@ do {
     userNumb = parseFloat(prompt("Inserisci un numero da 1 a 6"))
 }
 while(isNaN(userNumb) || userNumb > 6 || userNumb < 1)  
-
+console.log(`Numero utente ${userNumb}`)
 // Far generare un numero da 1 a 6 al computer
+
 let computerNumb = Math.floor(Math.random() * 6) + 1;
-console.log(computerNumb)
+console.log(`Numero computer ${computerNumb}`)
+
+// Verifico il vincitore in base a chi fa il punteggio più alto
 
 if(userNumb > computerNumb){
     alert("Hai vinto")
